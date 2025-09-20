1078c42a13d98069068d0b66d3756e4e47598148
Aditya Ranjan <132501526+ADI-0519@users.noreply.github.com>
2025-09-21 00:14:14 +0100

    Time: 39 ms (76.89%), Space: 28.2 MB (92.34%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
@@ -3,20 +3,20 @@ class Solution:
         
         skill.sort()
 
-        l,r = 0,len(skill)-1
-        prod = 0
-        summ = -1
+        l,r = 1,len(skill)-2
+        prod = skill[0] * skill[len(skill)-1]
+        total_skill = skill[0] + skill[len(skill)-1]
+
         while l <= r:
             
-            if (skill[l]+skill[r]) == summ or summ == -1:
-                summ = skill[l] + skill[r]
+            if (skill[l]+skill[r]) == total_skill:
+                prod += (skill[l] * skill[r])
+                l += 1
+                r -= 1
 
             else:
                 return -1
 
-            prod += (skill[l] * skill[r])
-            l += 1
-            r -= 1
 
 
         return prod
