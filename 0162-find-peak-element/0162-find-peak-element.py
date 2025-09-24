e205dfc149ac1f5fb1c4cc349ded221661409b84
Aditya Ranjan <132501526+ADI-0519@users.noreply.github.com>
2025-09-24 22:33:28 +0100

    Time: 0 ms (100%), Space: 17.8 MB (61.62%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -6,32 +6,13 @@ class Solution:
         
         l,r = 0,len(nums)-1
 
-        while l <= r:
+        while l < r:
             mid = l + (r-l) // 2
 
-            if mid < 1:
-                if nums[mid] > nums[mid+1]:
-                    return mid
-                else:
-                    l = mid + 1
-
-
-            if mid >= len(nums)-1:
-                if nums[mid-1] < nums[mid]:
-                    return mid
-
-                else:
-                    r = mid - 1
+            if nums[mid] > nums[mid+1]:
+                r = mid
 
             else:
-                
-                # check neighbours
+                l = mid + 1
 
-                if nums[mid-1] < nums[mid] and nums[mid] > nums[mid+1]:
-                    return mid
-
-                elif nums[mid-1] > nums[mid+1]:
-                    r = mid - 1
-
-                else:
-                    l = mid + 1
+        return l
